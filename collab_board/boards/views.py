34a4abafc3510824f7dcf1efbd4b72adfa73ff4e
mayank <123.ultron.1234@gmail.com>
2026-05-12 20:49:49 +0530

    feat: add validation and filtering for ListViewSet and CardViewSet

--- collab_board/boards/views.py
+++ collab_board/boards/views.py
@@ -3,20 +3,25 @@ from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError, NotFound
 from boards.models import Board, List, Card
 from boards.serializers import CardSerializer, BoardSerializer, ListSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+# Board View
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
 
+# List View
 class  ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
     
+    # List filter
     def get_queryset(self):
         queryset = super().get_queryset()
         board_id = self.request.query_params.get('board')
 
+        # validation and response
         if board_id is not None:
             try:
                 board_id = int(board_id)
@@ -30,6 +35,7 @@ class  ListViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(board_id=board_id)
         return queryset.order_by('id')
 
+# Card View
 class  CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
@@ -38,17 +44,47 @@ class  CardViewSet(viewsets.ModelViewSet):
         list_id = self.request.query_params.get('list')
         board_id = self.request.query_params.get('board')
 
+        # validation and response
         if list_id is not None:
             try:
                 list_id = int(list_id)
             except ValueError:
-                raise NotFound({"list": "Invalid list id"})
+                raise ValidationError({"list": "Id must be integer or number"})
+
+            # If valid id not found
+            if not List.objects.filter(id=list_id).exists():
+                raise NotFound({"list":f"List id {list_id} does not exists"})
+            
             queryset = queryset.filter(list_id=list_id)
         
         if board_id is not None:
             try:
                 board_id = int(board_id)
             except ValueError:
-                raise NotFound({"board": "Invalid board id"})
+                raise ValidationError({"board": "Id must be integer or number"})
+            
+            if not queryset.filter(list__board_id=board_id).exists():
+                raise NotFound({"board":f"Board id {board_id} does not exists"})
+            
             queryset = queryset.filter(list__board_id=board_id)
         return queryset.order_by('position')
+    
+
+    # move card between list
+    # update method
+    def update(self, request, *args, **kwargs):
+        card = self.get_object()
+
+        new_list = request.data.get('list')
+        new_position = request.data.get('position')
+
+        if new_list:
+            card.list_id = new_list
+
+        if new_position is not None:
+            card.position = new_position
+
+        card.save()
+
+        serializer = self.get_serializer(card)
+        return Response(serializer.data,)
