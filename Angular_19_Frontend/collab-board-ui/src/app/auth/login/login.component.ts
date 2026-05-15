568f257fcc92ae1c8e76c5b5c78ac17e0fc04840
mayank <123.ultron.1234@gmail.com>
2026-05-15 15:01:49 +0530

    feat: update login component to include registration link and RouterLink import

--- Angular_19_Frontend/collab-board-ui/src/app/auth/login/login.component.ts
+++ Angular_19_Frontend/collab-board-ui/src/app/auth/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
